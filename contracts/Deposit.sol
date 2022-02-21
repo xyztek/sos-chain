909e35ae30e05e880bf50191019f6142aae5a762
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-02-21 15:49:30 +0300

    Add mapping for allowed tokens with allow/disallow fns

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -3,4 +3,34 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Deposit {}
+contract Deposit {
+    /**
+     * @dev             map tokens allowed for deposit
+     * @param string    token symbol
+     * @param address   token contract address
+     */
+    mapping(bytes32 => address) public allowedTokens;
+
+    /**
+     * @dev             add a token to the list of allowed tokens
+     * @param string    token symbol
+     * @param address   token contract address
+     */
+    function allowTokenDeposit(bytes32 symbol_, address address_) public onlyOwner returns (bool) {
+        allowedTokens[symbol_] = address_;
+
+        return true;
+    }
+
+    /**
+     * @dev             remove a token from the list of allowed tokens
+     * @param string    token symbol
+     */
+    function disallowTokenDeposit(bytes32 symbol_) public onlyOwner returns (bool) {
+        require(allowedTokens[symbol_] != 0x0);
+
+        delete(allowedTokens[symbol_]);
+
+        return true;
+    }
+}
