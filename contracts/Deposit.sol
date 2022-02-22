a9d2aeb8e82c1caf083a664731614786861b8b12
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-02-22 10:20:13 +0300

    [WIP] Implement deposit contract

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -1,35 +1,58 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "hardhat/console.sol";
 
-contract Deposit {
-    /**
-     * @dev             map tokens allowed for deposit
-     * @param string    token symbol
-     * @param address   token contract address
-     */
+contract Deposit is Ownable {
+    using SafeERC20 for IERC20;
+
+    address private safeAddress;
     mapping(bytes32 => address) public allowedTokens;
+
+    event Support(address indexed from, bytes32 indexed symbol, uint256 value);
+
+    /**
+     * @dev             deposit into the safe
+     * @param symbol    token symbol (must be an allowed token)
+     * @param amount    amount to deposit
+     */
+    function deposit(bytes32 symbol, uint256 amount) external returns (bool) {
+        require(allowedTokens[symbol] != address(0x0));
+
+        IERC20(allowedTokens[symbol]).safeTransferFrom(msg.sender, safeAddress, amount);
+
+        emit Support(msg.sender, symbol, amount);
+
+        // TODO mint ERC721
+
+        return true;
+    }
+
+
+    // Functions to update the list of tokens supported for deposit
 
     /**
      * @dev             add a token to the list of allowed tokens
-     * @param string    token symbol
-     * @param address   token contract address
+     * @param symbol    token symbol
+     * @param _address  token contract address
      */
-    function allowTokenDeposit(bytes32 symbol_, address address_) public onlyOwner returns (bool) {
-        allowedTokens[symbol_] = address_;
+    function addToken(bytes32 symbol, address _address) public onlyOwner returns (bool) {
+        allowedTokens[symbol] = _address;
 
         return true;
     }
 
     /**
      * @dev             remove a token from the list of allowed tokens
-     * @param string    token symbol
+     * @param symbol    token symbol
      */
-    function disallowTokenDeposit(bytes32 symbol_) public onlyOwner returns (bool) {
-        require(allowedTokens[symbol_] != 0x0);
+    function removeToken(bytes32 symbol) public onlyOwner returns (bool) {
+        require(allowedTokens[symbol] != address(0x0));
 
-        delete(allowedTokens[symbol_]);
+        delete(allowedTokens[symbol]);
 
         return true;
     }
