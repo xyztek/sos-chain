ddf28fb759871481c0faa868778d69dfd4110306
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-03-03 14:11:18 +0300

    Simplify SOSRegistry

--- contracts/SOSRegistry.sol
+++ contracts/SOSRegistry.sol
@@ -2,35 +2,20 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "hardhat/console.sol";
 
 contract SOSRegistry is Ownable {
-    using SafeMath for uint256;
-
-    struct Record {
-        address owner;
-        address contractAddress;
-        uint256 version;
-    }
-
-    mapping(bytes32 => Record) private registry;
+    mapping(bytes32 => address) private registry;
 
     function register(bytes32 _name, address _address)
         public
         onlyOwner
         returns (bool)
     {
-        require(registry[_name].contractAddress == address(0x0));
+        require(registry[_name] == address(0x0));
 
-        Record memory record = Record({
-            owner: msg.sender,
-            contractAddress: _address,
-            version: 1
-        });
-
-        registry[_name] = record;
+        registry[_name] = _address;
 
         return true;
     }
@@ -40,15 +25,14 @@ contract SOSRegistry is Ownable {
         onlyOwner
         returns (bool)
     {
-        require(registry[_name].contractAddress != address(0x0));
+        require(registry[_name] != address(0x0));
 
-        registry[_name].contractAddress = _address;
-        registry[_name].version = registry[_name].version.add(1);
+        registry[_name] = _address;
 
         return true;
     }
 
-    function get(bytes32 _name) public view returns (address, uint256) {
-        return (registry[_name].contractAddress, registry[_name].version);
+    function get(bytes32 _name) public view returns (address) {
+        return registry[_name].contractAddress;
     }
 }
