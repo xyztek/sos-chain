612b8c1d7a44ef66103cd7b1bdd007bc2de1d900
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-03-05 11:18:43 +0300

    Enable Registry to return a list of registered contract names

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -6,8 +6,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract Registry is Ownable {
+    bytes32[] private contracts;
     mapping(bytes32 => address) private registry;
 
+    /**
+     * @dev              get a list of registered contract names
+     * @return           a list of registered contract names
+     */
+    function registered() public view returns (bytes32[] memory) {
+        return contracts;
+    }
+
+    /**
+     * @dev              get contract address
+     * @param _name      name of the contract
+     * @return           address of the queried contract
+     */
+    function get(bytes32 _name) public view returns (address) {
+        return registry[_name];
+    }
+
+    /**
+     * @dev              register contract address in the registry
+     * @param _name      name of the contract
+     * @param _address   address of the contract
+     * @return           boolean indicating result of the operation
+     */
     function register(bytes32 _name, address _address)
         public
         onlyOwner
@@ -16,10 +40,17 @@ contract Registry is Ownable {
         require(registry[_name] == address(0x0));
 
         registry[_name] = _address;
+        contracts.push(_name);
 
         return true;
     }
 
+    /**
+     * @dev              update contract address in the registry
+     * @param _name      name of the contract
+     * @param _address   address of the contract
+     * @return           boolean indicating result of the operation
+     */
     function update(bytes32 _name, address _address)
         public
         onlyOwner
@@ -30,9 +61,5 @@ contract Registry is Ownable {
         registry[_name] = _address;
 
         return true;
-    }
-
-    function get(bytes32 _name) public view returns (address) {
-        return registry[_name];
     }
 }
