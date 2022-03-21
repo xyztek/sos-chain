b8b7392db35705340285a2d88ffea0e23f2f8ddd
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-03-21 12:22:08 +0300

    Add self registation capability to Registered contracts

--- contracts/Registered.sol
+++ contracts/Registered.sol
@@ -24,4 +24,13 @@ contract Registered {
     function getAddress(bytes32 _name) internal view returns (address) {
         return registry.get(_name);
     }
+
+    /**
+     * @dev           self register
+     * @param  _name  name to register with
+     * @return        boolean indicating op result
+     */
+    function _register(bytes32 _name) internal returns (bool) {
+        return registry.register(_name, address(this));
+    }
 }
