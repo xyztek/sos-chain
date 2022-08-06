8b401eb4dba327c6acdf123f1ab75c9c96a9d5f4
Mete Can Eris <can.eris@xyzteknoloji.com>
2022-08-06 10:26:06 +0300

    Mark FundManager as Ownable

--- contracts/FundManager.sol
+++ contracts/FundManager.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FundManager is ERC1967Proxy {
+contract FundManager is ERC1967Proxy, Ownable {
     constructor(
         address _logic,
         address _admin,
